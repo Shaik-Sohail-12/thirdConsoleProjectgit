7d5b1b8069463415f0ce4a4a2db9b35171e1df54
shaik-sohail-12 <shaiksohailhussain786@gmail.com>
2022-11-21 23:31:06 +0530

    sdf

---
+++ src/main/java/com/kmit/demo/thirdConsoleProject/App.java
@@ -0,0 +1,13 @@
+package com.kmit.demo.thirdConsoleProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Third console project!" );
+    }
+}
